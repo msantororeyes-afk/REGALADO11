dd8a29b70ad14305370add8ed25daf5859f3530c
msantororeyes-afk <msantororeyes@gmail.com>
2025-10-23 09:59:18 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/globals.css";  // ✅ use your real stylesheet name
+import Navbar from "../components/Navbar";  // ✅ added this line
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />  {/* ✅ persistent dropdown navigation */}
+      <Component {...pageProps} />
+    </>
+  );
 }
